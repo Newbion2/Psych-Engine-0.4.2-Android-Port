596647b89b69bf9d2db9660f186ded53b40bd830
Newbion2 <96973667+Newbion2@users.noreply.github.com>
2022-01-02 17:57:39 +0900

    Add files via upload

---
+++ assets/preload/data/mason-moment/script.lua
@@ -0,0 +1,27 @@
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
+		setProperty('camGame.alpha',0)
+		setProperty('camHUD.alpha',0)
+		runTimer('wait',1,1);
+
+		allowCountdown = true;
+		return Function_Stop;
+	end
+
+
+
+		setProperty('camGame.alpha',1)
+		setProperty('camHUD.alpha',1)
+
+	return Function_Continue;
+
+end
+
+
+function onTimerCompleted(tag, loops, loopsLeft)
+
+		startVideo('MinusWakeScene');
+
+
+end
